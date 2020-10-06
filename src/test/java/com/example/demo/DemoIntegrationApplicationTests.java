16e6e1eac3ccdafd7f8dd42b9c4e5c37ab248bc5
Dimitri Bouras <d.bouras@neurocom.gr>
2020-10-06 23:01:45 +0300

    Adding unit test

--- src/test/java/com/example/demo/DemoIntegrationApplicationTests.java
+++ src/test/java/com/example/demo/DemoIntegrationApplicationTests.java
@@ -1,13 +1,72 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.net.URL;
 
-@SpringBootTest
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.model.MyResponse;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoIntegrationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/demo");
+    }
+
+    @Test
+    public void testPassValidator() throws JSONException {
+
+        HttpHeaders headers = new HttpHeaders();
+        // headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Accept", "application/json");
+        JSONObject myRequest = new JSONObject();
+        myRequest.put("msisdn", "55765");
+        myRequest.put("languageId", 5);
+        myRequest.put("transactionId", "00055765");
+        myRequest.put("notificationType", "SMS");
+
+        String string = myRequest.toString();
+        System.out.println(string);
+        MyResponse response = template.postForObject(base.toString(), string, MyResponse.class);
+        System.out.println(response);
+    }
+
+    @Test
+    public void testFailsValidator() throws JSONException {
+
+        HttpHeaders headers = new HttpHeaders();
+        // headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Accept", "application/json");
+        JSONObject myRequest = new JSONObject();
+        myRequest.put("msisdn", "");
+        myRequest.put("languageId", 5);
+        myRequest.put("transactionId", "00055765");
+        myRequest.put("notificationType", "SMS");
+
+        String string = myRequest.toString();
+        System.out.println(string);
+        MyResponse response = template.postForObject(base.toString(), string, MyResponse.class);
+        System.out.println(response);
+    }
 
 }
